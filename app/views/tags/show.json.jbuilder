95b371588d1b453d886d3dc9498fc9c9e32ec551
Dennis Schulz <dennis@port42.org>
2015-05-25 13:45:22 +0200

    adds documents and tags controller scaffolds

---
+++ app/views/tags/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @tag, :id, :name, :color, :icon, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :tags
+  resources :documents
+
+  get '/', to: redirect('/documents')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
